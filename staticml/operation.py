92d8a33dd560ae26c0bd56e51f56b97d4aabb335
CodingTrue <codingtrue@web.de>
2026-06-06 21:29:02 +0200

    Removed abstract definition from operation.py

--- staticml/operation.py
+++ staticml/operation.py
@@ -1,14 +1,12 @@
-from abc import ABC, abstractmethod
 from numbers import Number
 
 from staticml.buffer import Allocator
 from staticml.tensor import Tensor
 
 
-class Operation(ABC):
+class Operation:
     def __init__(self, arguments: list = None):
         self.arguments = arguments or []
 
-    @abstractmethod
     def allocate(self, allocator: Allocator):
         return
