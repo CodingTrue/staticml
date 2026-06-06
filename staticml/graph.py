2c7d83eb48caf7bb19fe3cc091a9f0f02443314e
CodingTrue <codingtrue@web.de>
2026-06-06 16:48:08 +0200

    Added tracking of tensor lifetimes

--- staticml/graph.py
+++ staticml/graph.py
@@ -8,8 +8,16 @@ class ComputeGraph:
     def __init__(self, root: Tensor):
         self.dynamic_tensors: list[Tensor] = []
         self.static_tensors: list[Tensor] = []
+        self.keepers: dict[Tensor, Tensor] = {}
+        self.liftetimes: dict[Tensor, list[Tensor]] = {}
 
         self._search(tensor=root)
+
+        for child, keeper in self.keepers.items():
+            if not keeper in self.liftetimes:
+                self.liftetimes[keeper] = []
+
+            self.liftetimes[keeper].append(child)
 
     def _search(self, tensor: Tensor):
         if tensor.is_static():
@@ -18,6 +26,8 @@ class ComputeGraph:
 
         for child in tensor.children:
             if not Tensor.is_tensor(child): continue
+            self.keepers[child] = tensor
+
             self._search(tensor=child)
 
         _add_if_missing(tensor, self.dynamic_tensors)
