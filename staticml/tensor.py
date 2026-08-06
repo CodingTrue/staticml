e2a51d6e79d70f821e6e3248291eaf004261024a
CodingTrue <codingtrue@web.de>
2026-08-06 23:27:52 +0200

    Added shapes to tensors

--- staticml/tensor.py
+++ staticml/tensor.py
@@ -1,10 +1,15 @@
+from dataclasses import dataclass
+
 import numpy as np
 
 from enum import Enum
-from typing import Any
+from typing import Any, Iterable
 
 
 type TensorArg = Tensor | Number
+
+def _get_common_size(args: Iterable[TensorArg]) -> int:
+    return max(arg.size for arg in args if Tensor.is_tensor(o=arg))
 
 class TensorOperation(Enum):
     ADD = '_add'
@@ -12,12 +17,24 @@ class TensorOperation(Enum):
     MUL = '_mul'
     DIV = '_div'
 
+@dataclass
+class TensorShape:
+    x: int = 1
+    y: int = 1
+    z: int = 1
+
 class Tensor:
-    def __init__(self, data: Any | None = None, args: tuple[TensorArg] | None = None):
+    def __init__(
+            self,
+            data: Any | None = None,
+            args: tuple[TensorArg] | None = None,
+            shape: TensorShape | None = None
+    ):
         self._data = np.asarray(data, dtype=np.float32)
         self._has_data = data is not None
 
         self._args = args or tuple()
+        self._shape = shape or TensorShape(x=_get_common_size(args=self._args) if not self._has_data else 0)
         self._is_static = len(self._args) == 0
 
     def __add__(self, other):
@@ -48,6 +65,10 @@ class Tensor:
     @property
     def has_data(self) -> bool:
         return self._has_data
+
+    @property
+    def shape(self) -> TensorShape:
+        return TensorShape(*self._data.shape) if self._has_data else self._shape
 
     @property
     def args(self) -> tuple[Any]:
